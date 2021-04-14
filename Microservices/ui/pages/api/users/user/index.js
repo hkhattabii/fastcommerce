c2f5d5d26c4f70e0b27d70f152f47a584f23d7a0
hkhattabii <hamzaa.khtb@gmail.com>
2021-04-14 22:35:35 +0200

    finetuning

--- Microservices/ui/pages/api/users/user/index.js
+++ Microservices/ui/pages/api/users/user/index.js
@@ -1,9 +1,10 @@
+import { AUTH_SERVICE } from "@/lib/serviceRegistry";
 import axios from "axios";
 
 export default async function UserHandler(req, res) {
     if (req.method === "GET") {
         try {
-            const {status, ...response} = await axios.get(`http://localhost:6001/user?field=${req.query.field}&value=${req.query.value}`)
+            const {status, ...response} = await axios.get(AUTH_SERVICE.BYFIELDANDVALUE(req.query.field,req.query.value))
             res.status(status).json(response.data)
         } catch ({response}) {
             res.status(response.status).json(response.data)
