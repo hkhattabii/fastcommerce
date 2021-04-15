5829196312a21b7df714ac8997b925f8205d2f3b
hkhattabii <hamzaa.khtb@gmail.com>
2021-04-15 11:43:09 +0200

    remove https

--- Microservices/services/auth-service/index.js
+++ Microservices/services/auth-service/index.js
@@ -18,7 +18,7 @@ app.listen(PORT, async () => {
   try {
     await mongoose.connect(
       process.env.ENV === 'production'
-        ? `mongodb://https://auth-db-5usc53cgfa-ew.a.run.app`
+        ? `mongodb://auth-db-5usc53cgfa-ew.a.run.app:27017`
         : `mongodb://localhost:27017`,
       {
         useCreateIndex: true,
