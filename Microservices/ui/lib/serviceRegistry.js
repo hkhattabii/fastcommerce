c61b8a4f02f430fb4a1d48b7a7cfecb146b8aea0
hkhattabii <hamzaa.khtb@gmail.com>
2021-04-21 23:33:43 +0200

    update test to test-db

--- Microservices/ui/lib/serviceRegistry.js
+++ Microservices/ui/lib/serviceRegistry.js
@@ -10,11 +10,11 @@ const AUTH_SERVICE = {
 };
 
 const PWD_REQ_SERVICE = {
-  ROOT: "http://localhost:5003",
-  BYIDANDCODE: (id, code) => `http://localhost:5003/${id}/${code}`,
+  ROOT: "http://167.172.41.60:5003",
+  BYIDANDCODE: (id, code) => `http://167.172.41.60:5003/${id}/${code}`,
   BYFIELDANDVALUE: (field, value) =>
-    `http://localhost:5003?field=${field}&value=${value}`,
-  BYEMAIL: (email) => `http://localhost:5003/byEmail?email=${email}`,
+    `http://167.172.41.60:5003?field=${field}&value=${value}`,
+  BYEMAIL: (email) => `http://167.172.41.60:5003/byEmail?email=${email}`,
 };
 
 const PRODUCT_SERVICE = {
@@ -26,50 +26,50 @@ const PRODUCT_SERVICE = {
 };
 
 const CART_SERVICE = {
-  BYUSER: (user_id) => `http://localhost:5007?user_id=${user_id}`,
+  BYUSER: (user_id) => `http://167.172.41.60:30003?user_id=${user_id}`,
   BYUSERANDPRODUCT: (user_id, product_id) =>
     `http://167.172.41.60:30007?user_id=${user_id}&product_id=${product_id}`,
   INCREASE: (user_id, product_id) =>
-    `http://localhost:5007/increase?user_id=${user_id}&product_id=${product_id}`,
+    `http://167.172.41.60:30003/increase?user_id=${user_id}&product_id=${product_id}`,
   DECREASE: (user_id, product_id) =>
-    `http://localhost:5007/decrease?user_id=${user_id}&product_id=${product_id}`,
-  CLEAR: (user_id) => `http://localhost:5007/clear?user_id=${user_id}`,
+    `http://167.172.41.60:30003/decrease?user_id=${user_id}&product_id=${product_id}`,
+  CLEAR: (user_id) => `http://167.172.41.60:30003/clear?user_id=${user_id}`,
   ROOT: (user_id, product_id) => {
     return user_id && product_id
-      ? `http://localhost:5007?user_id=${user_id}&product_id=${product_id}`
+      ? `http://167.172.41.60:30003?user_id=${user_id}&product_id=${product_id}`
       : user_id
-      ? `http://localhost:5007?user_id=${user_id}`
-      : `http://localhost:5007`;
+      ? `http://167.172.41.60:30003?user_id=${user_id}`
+      : `http://167.172.41.60:30003`;
   },
 };
 
 const BILL_SERVICE = {
   ROOT: (user_id, bill_id) => {
     return user_id && bill_id
-      ? `http://localhost:5011?user_id=${user_id}&bill_id=${bill_id}`
+      ? `http://167.172.41.60:30004?user_id=${user_id}&bill_id=${bill_id}`
       : user_id
-      ? `http://localhost:5011?user_id=${user_id}`
-      : `http://localhost:5011`;
+      ? `http://167.172.41.60:30004?user_id=${user_id}`
+      : `http://167.172.41.60:30004`;
   },
 };
 
 const DELIVERY_SERVICE = {
   ROOT: (user_id, delivery_id, bill_id) => {
     return delivery_id && bill_id
-      ? `http://localhost:5015?delivery_id=${delivery_id}&bill_id=${bill_id}`
+      ? `http://167.172.41.60:30005?delivery_id=${delivery_id}&bill_id=${bill_id}`
       : user_id
-      ? `http://localhost:5015?user_id=${user_id}`
-      : `http://localhost:5015`;
+      ? `http://167.172.41.60:30005?user_id=${user_id}`
+      : `http://167.172.41.60:30005`;
   },
 };
 
 const BACKUP_SERVICE = {
   ROOT: (user_id, product_id) => {
     return user_id && product_id
-      ? `http://localhost:5013?user_id=${user_id}&product_id=${product_id}`
+      ? `http://167.172.41.60:30006?user_id=${user_id}&product_id=${product_id}`
       : user_id
-      ? `http://localhost:5013?user_id=${user_id}`
-      : `http://localhost:5013`;
+      ? `http://167.172.41.60:30006?user_id=${user_id}`
+      : `http://167.172.41.60:30006`;
   },
 };
 
